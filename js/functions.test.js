ceb9bf0901fed8348a1e24dd40afc48fa19bff9b
shuler18 <46794269+shuler18@users.noreply.github.com>
2019-01-25 17:16:38 -0800

    added bad test for travis

--- js/functions.test.js
+++ js/functions.test.js
@@ -7,3 +7,7 @@ test('Adds 2+2 to ensure the answer really is 4', () => {
 test('Adds -2+2 to ensure the answer really is 0', () => {
 	expect(sums(-2,2)).toBe(0);
 });
+
+test('Adds 2+2 but fails as the check expects the result to be 3', () => {
+	expect(sums(2,2)).toBe(3);
+});
